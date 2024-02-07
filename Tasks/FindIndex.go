7c0034bcbbecf8577c701e162dec3076d7d3c02b
Fatima Noor <65679332+Fatimanoor123@users.noreply.github.com>
2024-02-07 10:34:03 +0500

    Create FindIndex.go

---
+++ Tasks/FindIndex.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var slc []int
+	var element int
+	slc = append(slc, 21)
+	slc = append(slc, 12)
+	slc = append(slc, 10)
+	slc = append(slc, 1)
+	slc = append(slc, 14)
+	fmt.Println("The elements in Slice are: ", slc)
+	fmt.Println("Do you want to check the index of any element, then enter element")
+
+	fmt.Scanln(&element)
+
+	for i := 0; i < 5; i++ {
+		if slc[i] == element {
+			fmt.Println("The address of desired element is as follows: ", &slc[i])1
+		}
+	}
+
+}
