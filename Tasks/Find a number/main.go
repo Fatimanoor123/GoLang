40189613cb0e14e7e58c0ac1e5451cc97793d0d4
Fatima Noor <65679332+Fatimanoor123@users.noreply.github.com>
2024-02-07 10:15:24 +0500

    Add files via upload

---
+++ Tasks/Find a number/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var a []int
+	a = append(a, 23)
+	a = append(a, 2)
+	a = append(a, 311)
+	a = append(a, 42)
+	a = append(a, 51)
+	fmt.Println("Values in slice are", a)
+	sort.Ints(a)
+	fmt.Println("the maximum value is: ", a[len(a)-1])
+
+}
