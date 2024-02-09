e822af3e726d1d0a6e6c09d20909215312dc53ac
Fatima Noor <65679332+Fatimanoor123@users.noreply.github.com>
2024-02-09 13:02:18 +0500

    Update FindIndex.go

--- Tasks/FindIndex.go
+++ Tasks/FindIndex.go
@@ -1,25 +1,38 @@
-//find index of specific element in slice
 package main
 
 import "fmt"
 
 func main() {
-	var slc []int
-	var element int
-	slc = append(slc, 21)
-	slc = append(slc, 12)
-	slc = append(slc, 10)
-	slc = append(slc, 1)
-	slc = append(slc, 14)
-	fmt.Println("The elements in Slice are: ", slc)
-	fmt.Println("Do you want to check the index of any element, then enter element")
+    var slc []int
+    var number int
 
-	fmt.Scanln(&element)
+    fmt.Println("Enter numbers for the slice (enter -1 when done):")
+    
+    // Pre-setting a maximum number of inputs for simplicity
+    for i := 0; i < 20; i++ { // Assuming the user won't enter more than 20 numbers
+        fmt.Scan(&number)
+        if number == -1 {
+            break // Exit the loop if the sentinel value is encountered
+        }
+        slc = append(slc, number)
+    }
 
-	for i := 0; i < 5; i++ {
-		if slc[i] == element {
-			fmt.Println("The address of desired element is as follows: ", &slc[i])1
-		}
-	}
+    fmt.Println("Enter the number you want to find:")
+    fmt.Scan(&number) // Read the number to find
 
+    found := false // Flag to indicate if the number is found
+    index := -1 // Variable to store the index of the found number
+    for i := 0; i < len(slc); i++ { // Iterate through the slice
+        if slc[i] == number { // Check if current element is the number we're looking for
+            found = true
+            index = i
+            break // Stop the loop if number is found
+        }
+    }
+
+    if found { // If the number was found, print its index
+        fmt.Printf("Number found at index: %d\n", index)
+    } else { // If the number was not found, indicate so
+        fmt.Println("Number not found in the slice.")
+    }
 }
