5f76f42f1bb7c43a3e4e4d38b4bfdbcd8e7f90c0
Fatima Noor <65679332+Fatimanoor123@users.noreply.github.com>
2024-02-09 11:44:52 +0500

    Create PracticeMultiplevalues.go

---
+++ PracticeMultiplevalues.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func vals ()(int, int) {
+    
+	return 48,54
+
+
+}
+
+func main() {
+  a,b:=vals()    
+  fmt.Println("The first value is ", a)
+  fmt.Println("The second value is: ", b)
+
+
+}
